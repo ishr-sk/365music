73d432de9a8a98af3ca02a4a9e3432d373af7db2
ishr-sk <kmrttnr1208@gmail.com>
2021-08-06 14:52:42 +0000

    [追加]postのedit/destroyを実装+レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   
-  resources :posts, only: [:new, :create, :index, :show, :destroy]
+  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   
 end
