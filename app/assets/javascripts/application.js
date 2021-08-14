eff0f6211e61558a6920afdc5247e7393169de5c
ishr-sk <kmrttnr1208@gmail.com>
2021-08-14 14:26:54 +0000

    [修正]ヘッダー/topを編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,23 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+/* 
+  Description: Animates the header upon scroll
+  
+*/
+/*global $*/
+$(function(){
+  var header = $("header"),
+      yOffset = 0,
+      triggerPoint = 150;
+  $(window).scroll(function(){
+    yOffset = $(window).scrollTop();
+    
+    if(yOffset >= triggerPoint){
+      header.addClass("minimized");
+    }else{
+      header.removeClass("minimized");
+    }
+    
+  });
+});
