fda9ab9c26575eae50f232515647a6a13e52c6d0
ishr-sk <kmrttnr1208@gmail.com>
2021-08-17 05:12:05 +0000

    [追加]config/environments/production.rbにMailer設定を記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "music365_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      port: 587,
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      user_name: ENV['SEND_MAIL'],
+      password: ENV['SEND_MAIL_PASSWORD'],
+      authentication: 'login',
+      enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
