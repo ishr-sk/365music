6513f7fae5ad278729767348d18b61b1ede3b68b
ishr-sk <kmrttnr1208@gmail.com>
2021-08-17 15:13:42 +0000

    [修正]DM機能のエラーを改善/controllerを修正

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,15 +1,23 @@
 class DirectMessagesController < ApplicationController
   def show
+    # どのユーザと会話するか取得
     @user = User.find(params[:id])
+    # カレントユーザーのentry(中間テーブル)にあるroom_idの値をroomsに代入
     rooms = current_user.entries.pluck(:room_id)
+    # entryモデルから[user_idがチャット相手のidと一致するもの]と
+    # [room_idが上記roomsのどれかに一致するもの]をentryに代入
     entries = Entry.find_by(user_id: @user.id, room_id: rooms)
+    # もしentryが空ではなかったら
     unless entries.nil?
+      # @roomに上記entryのroomを代入
       @room = entries.room
     else
+      # それ以外は新しくroomを作る
       @room = Room.new
       @room.save
-      UserRoom.create(user_id: current_user.id, room_id: @room.id)
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
+      # カレントユーザー分とチャット相手分のentry作る
+      Entry.create(user_id: current_user.id, room_id: @room.id)
+      Entry.create(user_id: @user.id, room_id: @room.id)
     end
     @direct_messages = @room.direct_messages
     @direct_message = DirectMessage.new(room_id: @room.id)
