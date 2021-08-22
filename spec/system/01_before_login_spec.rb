5e6af50065bb29d30e15385f5c53a5e1666245b6
ishr-sk <kmrttnr1208@gmail.com>
2021-08-22 16:14:27 +0000

    [追加]RSpec(user/post)を追加

---
+++ spec/system/01_before_login_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe '[STEP1] ユーザログイン前のテスト' do
+  describe 'トップ画面のテスト' do
+    before do
+      visit root_path
+    end
+  end
+
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_user_registration_path
+    end
+
+    context '新規登録成功のテスト' do
+      before do
+        fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+      end
+    end
+  end
+  
+  describe 'ユーザログイン' do
+    let(:user) { create(:user) }
+  
+    before do
+      visit new_user_session_path
+    end
+    context 'ログイン成功のテスト' do
+      before do
+        fill_in 'user[name]', with: user.name
+        fill_in 'user[password]', with: user.password
+        click_button 'Log in'
+      end
+    end
+    
+    context 'ログイン失敗のテスト' do
+      before do
+        fill_in 'user[name]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'Log in'
+      end
+    end
+  end
+end
