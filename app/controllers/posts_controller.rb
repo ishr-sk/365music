514b9a6f7d4330434f2549f76d1681e9e50e97e0
ishr-sk <kmrttnr1208@gmail.com>
2021-08-25 08:00:37 +0000

    [修正]posts/indexの表示数を変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def index
     # フォローしているユーザーと自分の投稿を表示
-    @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).order("created_at DESC").page(params[:page]).per(8)
+    @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).order("created_at DESC").page(params[:page]).per(6)
   end
 
   def show
